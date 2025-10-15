5c7ef44452052d15a45280e7a71c1ceb3951fb95
Thomas DeSantis <40611847+ThomasDeSantis@users.noreply.github.com>
2025-10-15 18:19:27 -0400

    Updated hex gen

--- chiaroscuro/mapgen/ChGenerateHex/ChGenerateHex/GenerateHexMain.cs
+++ chiaroscuro/mapgen/ChGenerateHex/ChGenerateHex/GenerateHexMain.cs
@@ -1,8 +1,10 @@
 ﻿using ChGenerateHex;
-using Libraries;
+using DatabaseLibraries;
 using MapData;
 using System;
 using System.Reflection.Metadata;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 class GenerateHexMain
 {
@@ -27,11 +29,11 @@ class GenerateHexMain
 
         Console.WriteLine($"Generating hex: x {args[0]}, y {args[1]} Debug: {args[2]}");
 
-        int originX = int.Parse(args[0]);
-        int originY = int.Parse(args[1]);
+        (int,int) clientOrigin = (int.Parse(args[0]), int.Parse(args[1]));
+        (int,int) origin = Hex.hexCoordsToPointCoords(clientOrigin, 100,85);
         bool debug = bool.Parse(args[2]);
 
-        List<(int, int)> hexPoints = Geometry.generateHexagonPoints((originX, originY), 100, 85);
+        List<(int, int)> hexPoints = Geometry.generateHexagonPoints(clientOrigin, 100, 86);
 
         HashSet<ChPoint> chPoints = new HashSet<ChPoint>();
         for (int i = 0; i < hexPoints.Count; i++)
@@ -39,24 +41,27 @@ class GenerateHexMain
             chPoints.Add(new ChPoint(hexPoints[i].Item1, hexPoints[i].Item2));
         }
 
-        Hex newHex = new Hex(chPoints, (originX, originY));
+        Hex newHex = new Hex(chPoints, origin, clientOrigin);
 
         Console.WriteLine(newHex);
         Console.WriteLine($"Normalized Bounds: (0,0) to ({newHex.getNormalizedBounds().Item1}, {newHex.getNormalizedBounds().Item2})");
         Console.WriteLine($"Min X: {newHex.getHexBounds().Item1.Item1} Max X: {newHex.getHexBounds().Item2.Item1}");
         Console.WriteLine($"Min Y: {newHex.getHexBounds().Item1.Item2} Max Y: {newHex.getHexBounds().Item2.Item2}");
-        Console.WriteLine(ChPointSerializeJSON.serializeChPointJSON(newHex.Points, (originX, originY)));
+        //Console.WriteLine(ChPointSerializeJSON.serializeChPointJSON(newHex.Points, origin));
 
 
-        if (debug)
+        if (debug && false)
         {
             ((int, int), (int, int)) hexBounds = newHex.getHexBounds();
             Console.WriteLine($"Hex Bounds: Top Left ({hexBounds.Item1.Item1}, {hexBounds.Item1.Item2}) Bottom Right ({hexBounds.Item2.Item1}, {hexBounds.Item2.Item2})");
             ChDrawHex.drawHexBounds(newHex, debugImageDirectory + "\\hex-bounds.png");
         }
-
-        AppSettings config = new AppSettings("C:\\Users\\Thomas\\Documents\\fantasy-colonialism", "appsettings.json");
-        string connectionString = config.getSqlConnectionString("DefaultConnection");
+        Console.WriteLine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName);/*
+        AppSettings config = new AppSettings(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, "appsettings.json");*/
+        IConfigurationBuilder builder = new ConfigurationBuilder().SetBasePath(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName).AddJsonFile("appsettings.json");
+        IConfiguration config = builder.Build();
+        //string connectionString = config.getSqlConnectionString("DefaultConnection");
+        string connectionString = config.GetConnectionString("DatabaseConnection");
         Console.WriteLine($"Connection String: {connectionString}");
 
         var dbCon = DBConnection.Instance();
